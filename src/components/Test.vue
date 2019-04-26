205dc7e6c37030434dc4bfb13bd99d8dc1d1f792
Vincent Rasquier <vincent.rasquier@gmail.com>
2019-04-26 17:07:24 +0200

    toto

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,13 +1,25 @@
 <template>
-  <b-button variant="primary" @click="sendTest" />
+  <b-button
+    variant="primary"
+    @click="sendTest"
+  >
+    Send
+  </b-button>
 </template>
 
 <script>
 export default {
   mounted() {
     const source = new EventSource('http://localhost:8888/events');
+    console.log(source);
+    source.onopen	= function(event) {
+      console.log("open", event);
+    };
     source.onmessage = function(event) {
-      console.log(event);
+      console.log("message", event);
+    };
+    source.onerror = function(event) {
+      console.log("error", event);
     };
   },
   methods: {
